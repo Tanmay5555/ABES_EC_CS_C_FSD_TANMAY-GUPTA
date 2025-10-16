98bfc9ce110cff67cd15d41e83126f366085d22c
tanmay <varshneytanmay75@gmail.com>
2025-10-16 12:44:05 +0530

    initial

---
+++ day10/promise.js
@@ -0,0 +1,12 @@
+const ordercoffee = () => {
+    setTimeout(() => {
+        console.log("Make coffee ordered");
+        setTimeout(() => {
+            console.log("Serve coffee to customer");
+        }
+    }
+    const orderCoffee=()=>{
+        WaitFun(4000,"Make some order");
+        await WaitFun(2000,'')
+        .
+    }
