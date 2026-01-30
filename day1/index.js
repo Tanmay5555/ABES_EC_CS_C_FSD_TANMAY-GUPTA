0cbde3430ac934bebb680d65c67fc2b67925c378
tanmay <varshneytanmay75@gmail.com>
2026-01-30 09:55:51 +0530

    swecond

--- day1/index.js
+++ day1/index.js
@@ -6,9 +6,31 @@ const myPromise = new Promise((resolve, reject) => {
         reject("You failed the exam");
     }
 });
-myPromise.then((message) => {
-    console.log(message);
-}).catch((message) => {
+const f1= async () =>{
+    try {
+ const messages= await myPromise;
+    console.log(messages);
+}
+    catch (message) {
+        console.log(message);
+    }
+}
+//f1();    
+const getData= async () =>{
+    try{
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log("Error fetching data:", error);
+    }
+}
+fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+// myPromise.then((message) => {
+//     console.log(message);
+// }).catch((message) => {
 
 
 
